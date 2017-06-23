e19dd68ee7ad6f02fc36d784ec8a649411bf1fa8
Wusch <florian.otto@mindbird.de>
2017-06-23 04:36:40 +0200

    Initial

--- src/Module/Booking.php
+++ src/Module/Booking.php
@@ -70,6 +70,7 @@ class Booking extends Module
         $this->fields['startDate'] = $field;
 
         $field = new FormTextField();
+        $field->type = 'time';
         $field->template = 'form_room_reservation_textfield';
         $field->name = 'startTime';
         $field->label = 'Startzeit';
@@ -84,6 +85,7 @@ class Booking extends Module
         $this->fields['endDate'] = $field;
 
         $field = new FormTextField();
+        $field->type = 'time';
         $field->template = 'form_room_reservation_textfield';
         $field->name = 'endTime';
         $field->label = 'Endzeit';
