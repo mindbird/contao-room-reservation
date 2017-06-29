5b2a7875cc49d3bec73326544ac142cedcad78eb
Wusch <florian.otto@mindbird.de>
2017-06-29 09:39:03 +0200

    Initial

--- src/Module/Booking.php
+++ src/Module/Booking.php
@@ -42,7 +42,7 @@ class Booking extends Module
         if (Input::post($this->fields['formSubmit']->name) == $this->fields['formSubmit']->value) {
             $startDate = \DateTime::createFromFormat('d.m.YH:s', Input::post('startDate') . Input::post('startTime'));
             $endDate = \DateTime::createFromFormat('d.m.YH:s', Input::post('endDate') . Input::post('endTime'));
-            $result = $db->prepare("SELECT id FROM tl_calendar_events WHERE startDate <= ? AND endDate >= ? AND pid = ?")->execute($endDate->format('U') + 30 * 60,
+            $result = $db->prepare("SELECT id FROM tl_calendar_events WHERE startTime <= ? AND endTime >= ? AND pid = ?")->execute($endDate->format('U') + 30 * 60,
                 $startDate->format('U'), $this->room_event_archive);
             if ($result->numRows == 0) {
                 $cem = new CalendarEventsModel();
