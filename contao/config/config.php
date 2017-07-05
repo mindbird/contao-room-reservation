26118f6741961a59a92389560093d4d66e4a0d72
Wusch <florian.otto@mindbird.de>
2017-07-05 13:32:56 +0200

    Initial

--- contao/config/config.php
+++ contao/config/config.php
@@ -7,3 +7,22 @@ array_insert($GLOBALS ['FE_MOD'] ['events'], 1, array(
     'room_reservation' => 'RoomReservation\Module\Booking'
 ));
 
+$GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['room_reservation'] = array
+(
+    // Type
+    'booking_confirmation'   => array
+    (
+        // Field in tl_nc_language
+        'recipients'           => array('member_email', 'admin_email'),
+        'email_subject'        => array('domain', 'member_*', 'admin_email'),
+        'email_text'           => array('domain', 'member_*', 'admin_email'),
+        'email_html'           => array('domain', 'member_*', 'admin_email'),
+        'file_name'            => array('domain', 'member_*', 'admin_email'),
+        'file_content'         => array('domain', 'member_*', 'admin_email'),
+        'email_sender_name'    => array('admin_email', 'form_*'),
+        'email_sender_address' => array('admin_email', 'form_*'),
+        'email_recipient_cc'   => array('admin_email', 'member_*'),
+        'email_recipient_bcc'  => array('admin_email', 'member_*'),
+        'email_replyTo'        => array('admin_email', 'member_*')
+    )
+);
