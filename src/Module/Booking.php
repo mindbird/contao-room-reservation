caed0519e30d142eb9546bdcd9977314ab7c760a
Wusch <florian.otto@mindbird.de>
2017-08-30 10:34:13 +0200

    Add repeat of event

--- src/Module/Booking.php
+++ src/Module/Booking.php
@@ -27,6 +27,11 @@ class Booking extends Module
 
     public function generate()
     {
+        if (Input::post('action') == 'checkAvailability' && Input::post($this->fields['formSubmit']->name) == $this->fields['formSubmit']->value) {
+            print json_encode($this->checkAvailabilityAjax(Input::post('repeat'), Input::post('startDate'), Input::post('startTime'), Input::post('endDate'), Input::post('endTime')));
+            die();
+        }
+
         $GLOBALS['TL_CSS'][] = 'system/modules/room_reservation/assets/css/datepicker.min.css';
         $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/room_reservation/assets/js/datepicker.min.js|static';
         $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/room_reservation/assets/js/datepicker-de.js|static';
@@ -39,43 +44,52 @@ class Booking extends Module
     {
         $this->initFields();
         $user = FrontendUser::getInstance();
-        $db = Database::getInstance();
 
         if (Input::post($this->fields['formSubmit']->name) == $this->fields['formSubmit']->value) {
-            $startDate = \DateTime::createFromFormat('d.m.YH:i', Input::post('startDate') . Input::post('startTime'));
-            $endDate = \DateTime::createFromFormat('d.m.YH:i', Input::post('endDate') . Input::post('endTime'));
-            $result = $db->prepare("SELECT id FROM tl_calendar_events WHERE startTime <= ? AND endTime >= ? AND pid = ?")->execute($endDate->format('U') + $this->room_reservation_time_between_entries * 60,
-                $startDate->format('U'), $this->room_event_archive);
-            if ($result->numRows == 0) {
-                $cem = new CalendarEventsModel();
-                $cem->pid = $this->room_event_archive;
-                $cem->startDate = $startDate->format('U');
-                $cem->startTime = $startDate->format('U');
-                $cem->endDate = $endDate->format('U');
-                $cem->endTime = $endDate->format('U');
-                $cem->title = Input::post('eventTitle');
-                $cem->published = true;
-                $cem->addTime = true;
-                $cem->member = $user->id;
-                $cem->save();
-
-                if ($this->room_reservation_notification != 0) {
-                    $token = [
-                        'room_start_date' => $startDate->format('d.m.Y H:i'),
-                        'room_end_date' => $endDate->format('d.m.Y H:i'),
-                        'room_event_title' => Input::post('eventTitle'),
-
-                    ];
-                    $notification = Notification::findByPk($this->room_reservation_notification);
-                    if (null !== $notification) {
-                        $notification->send($token);
-                    }
-                }
-
-                $this->jumpToOrReload($this->jumpTo, '/month/' . $startDate->format('Ym'));
-            } else {
-                $this->Template->noTimeslotAvailable = true;
+            $repeat = 0;
+            if (Input::post('repeatTimes') > 0) {
+                $repeat = Input::post('repeatTimes');
             }
+
+            for ($i = 0; $i <= $repeat; $i++) {
+                $addInterval = new \DateInterval('P' . $i * 7 . 'D');
+                $startDate = \DateTime::createFromFormat('d.m.YH:i',Input::post('startDate') . Input::post('startTime'));
+                $endDate = \DateTime::createFromFormat('d.m.YH:i', Input::post('endDate') . Input::post('endTime'));
+                $startDate->add($addInterval);
+                $endDate->add($addInterval);
+
+                if ($this->checkAvailability($startDate, $endDate)) {
+                    $cem = new CalendarEventsModel();
+                    $cem->pid = $this->room_event_archive;
+                    $cem->startDate = $startDate->format('U');
+                    $cem->startTime = $startDate->format('U');
+                    $cem->endDate = $endDate->format('U');
+                    $cem->endTime = $endDate->format('U');
+                    $cem->title = Input::post('eventTitle');
+                    $cem->published = true;
+                    $cem->addTime = true;
+                    $cem->member = $user->id;
+                    $cem->save();
+                }
+            }
+
+            if ($this->room_reservation_notification != 0) {
+                $startDate = \DateTime::createFromFormat('d.m.YH:i',Input::post('startDate') . Input::post('startTime'));
+                $endDate = \DateTime::createFromFormat('d.m.YH:i', Input::post('endDate') . Input::post('endTime'));
+                $token = [
+                    'room_start_date' => $startDate->format($GLOBALS['TL_CONFIG']['datimFormat']),
+                    'room_end_date' => $endDate->format($GLOBALS['TL_CONFIG']['datimFormat']),
+                    'room_repeat' => $repeat > 0 ? true : false,
+                    'room_repeat_times' => $repeat,
+                    'room_event_title' => Input::post('eventTitle'),
+                ];
+                $notification = Notification::findByPk($this->room_reservation_notification);
+                if (null !== $notification) {
+                    $notification->send($token);
+                }
+            }
+
+            $this->jumpToOrReload($this->jumpTo, '/month/' . $startDate->format('Ym'));
 
         } elseif (Input::get('date') != '') {
             $date = substr(Input::get('date'), 6, 2) . '.' . substr(Input::get('date'), 4,
@@ -114,6 +128,7 @@ class Booking extends Module
         $field = new FormTextField();
         $field->template = 'form_room_reservation_textfield';
         $field->name = 'eventTitle';
+        $field->id = 'eventTitle';
         $field->label = 'Titel der Veranstaltung';
         $field->value = Input::post('eventTitle');
         $this->fields['eventTitle'] = $field;
@@ -121,6 +136,7 @@ class Booking extends Module
         $field = new FormTextField();
         $field->template = 'form_room_reservation_textfield';
         $field->name = 'startDate';
+        $field->id = 'startDate';
         $field->label = 'Startdatum';
         $field->mandatory = true;
         $field->value = Input::post('startDate');
@@ -129,6 +145,7 @@ class Booking extends Module
         $field = new FormSelectMenu();
         $field->template = 'form_room_reservation_select';
         $field->name = 'startTime';
+        $field->id = 'startTime';
         $field->label = 'Startzeit';
         $field->mandatory = true;
         $field->options = $timeslot;
@@ -138,6 +155,7 @@ class Booking extends Module
         $field = new FormTextField();
         $field->template = 'form_room_reservation_textfield';
         $field->name = 'endDate';
+        $field->id = 'endDate';
         $field->label = 'Enddatum';
         $field->mandatory = true;
         $field->value = Input::post('endDate');
@@ -146,11 +164,31 @@ class Booking extends Module
         $field = new FormSelectMenu();
         $field->template = 'form_room_reservation_select';
         $field->name = 'endTime';
+        $field->id = 'endTime';
         $field->label = 'Endzeit';
         $field->mandatory = true;
         $field->options = $timeslot;
         $field->value = Input::post('endTime');
         $this->fields['endTime'] = $field;
+
+        $field = new FormCheckBox();
+        $field->template = 'form_room_reservation_checkbox';
+        $field->name = 'repeat';
+        $field->id = 'repeat';
+        $field->value = Input::post('repeat');
+        $field->options = array(
+            array('value' => '1', 'label' => 'Soll der Termin wiederholt werden?', 'mandatory' => true)
+        );
+        $this->fields['repeat'] = $field;
+
+        $field = new FormTextField();
+        $field->template = 'form_room_reservation_textfield';
+        $field->name = 'repeatTimes';
+        $field->id = 'repeatTimes';
+        $field->label = 'Wie viele Wochen soll der Termin wiederholt werden?';
+        $field->mandatory = true;
+        $field->value = Input::post('repeatTimes') > 0 ? Input::post('repeatTimes') : 0;
+        $this->fields['repeatTimes'] = $field;
 
         $pageAgbModel = \PageModel::findByPk($this->room_reservation_page_agb);
         if ($pageAgbModel) {
@@ -162,6 +200,7 @@ class Booking extends Module
         $field = new FormCheckBox();
         $field->template = 'form_room_reservation_checkbox';
         $field->name = 'agb';
+        $field->id = 'agb';
         $field->value = Input::post('agb');
         $field->options = array(
             array('value' => 'Hiermit stimme ich den AGB zu', 'label' => $label, 'mandatory' => true)
@@ -178,5 +217,51 @@ class Booking extends Module
             'label' => str_pad($hour, 2, 0, STR_PAD_LEFT) . ':' . $minute,
             'value' => str_pad($hour, 2, 0, STR_PAD_LEFT) . ':' . $minute
         );
+    }
+
+    /**
+     * @param \DateTime $startDate
+     * @param \DateTime $endDate
+     * @return boolean
+     */
+    private function checkAvailability($startDate, $endDate)
+    {
+        $db = Database::getInstance();
+        $result = $db->prepare("SELECT id FROM tl_calendar_events WHERE startTime <= ? AND endTime >= ? AND pid = ?")->execute($endDate->format('U') + $this->room_reservation_time_between_entries * 60,
+            $startDate->format('U'), $this->room_event_archive);
+
+        return $result->numRows == 0;
+    }
+
+    private function checkAvailabilityAjax(
+        $repeat,
+        $startDate,
+        $startTime,
+        $endDate,
+        $endTime
+    ) {
+        $return = [
+            'status' => true,
+            'msg' => '',
+            'events' => []
+        ];
+
+        for ($i = 0; $i <= $repeat; $i++) {
+            $addInterval = new \DateInterval('P' . $i * 7 . 'D');
+            $startDateTime = \DateTime::createFromFormat('d.m.YH:i', $startDate . $startTime);
+            $startDateTime->add($addInterval);
+            $endDateTime = \DateTime::createFromFormat('d.m.YH:i', $endDate . $endTime);
+            $endDateTime->add($addInterval);
+            $availabilityEvent = $startDateTime->format($GLOBALS['TL_CONFIG']['datimFormat']) . ' bis ' . $endDateTime->format($GLOBALS['TL_CONFIG']['datimFormat']) . ' - ';
+            if (!$this->checkAvailability($startDateTime, $endDateTime)) {
+                $availabilityEvent .= '<span class="error">nicht verfügbar</span>';
+                $return['status'] = false;
+            } else {
+                $availabilityEvent .= '<span>verfügbar</span>';
+            }
+            $return['events'][] = $availabilityEvent;
+        }
+
+        return $return;
     }
 }
